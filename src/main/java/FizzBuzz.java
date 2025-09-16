61062014c327be7368b710af28f9f277977f0681
etilE-mI <treebro2005@gmail.com>
2025-09-16 12:57:12 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -9,6 +9,19 @@ public class FizzBuzz {
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
     public static void main(String[] args) {
-        // TODO
+        //TODO
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println("");
+            }
+        }
     }
 }
